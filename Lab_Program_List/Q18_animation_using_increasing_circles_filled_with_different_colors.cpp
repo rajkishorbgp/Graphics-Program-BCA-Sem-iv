d113b40d866454b4a737678551ba789405370f7f
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-07-23 17:47:56 +0530

    18. Write a Program to draw animation using increasing circles filled with different colors and     patterns. (use setfillstyle function).

--- Lab_Program_List/Q18_animation_using_increasing_circles_filled_with_different_colors.cpp
+++ Lab_Program_List/Q18_animation_using_increasing_circles_filled_with_different_colors.cpp
@@ -4,3 +4,30 @@
         Write a Program to draw animation using increasing circles filled with different
         colors and patterns. (use setfillstyle function).
 */
+
+#include <graphics.h>
+int main()
+{
+    int d = 25, i, s = 1, j;
+    initwindow(800, 800, "Raj Kishor");
+    for (i = 1; i <= 15; i++)
+    {
+        setcolor(i);
+        for (j = 0; j < 5; j++)
+            circle(400, 400, d - j);
+        setfillstyle(s, i);
+        for (j = 0; j < 5; j++)
+            circle(400, 400, d - 25 - j);
+        floodfill(400 - d + 10, 400, i);
+        if (s == 11)
+        {
+            s = 1;
+        }
+        d += 25;
+        s++;
+        delay(200);
+    }
+    printf("raj");
+    getch();
+    closegraph();
+}
