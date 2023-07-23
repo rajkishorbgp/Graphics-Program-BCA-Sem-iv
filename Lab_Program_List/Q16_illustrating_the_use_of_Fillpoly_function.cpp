49513fc3510a0b1b6613a5f52a811753f5aa4d8f
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-07-23 17:43:26 +0530

    16. Write a Program that illustrating the use of Fillpoly function.

--- Lab_Program_List/Q16_illustrating_the_use_of_Fillpoly_function.cpp
+++ Lab_Program_List/Q16_illustrating_the_use_of_Fillpoly_function.cpp
@@ -2,3 +2,18 @@
                     Program: 16
     Write a Program that illustrating  the use of Fillpoly function.
 */
+#include <graphics.h>
+int main()
+{
+    int p, point[] = {400, 200, 350, 250, 370, 250, 300, 350, 360, 350,
+                      360, 350, 250, 450, 380, 450, 380, 500, 420, 500,
+                      420, 450, 550, 450, 440, 350, 500, 350, 430, 250,
+                      450, 250, 400, 200};
+    initwindow(900, 800, "Rajkishor");
+
+    p = (sizeof(point) / sizeof(int)) / 2;
+    fillpoly(p, point);
+    getch();
+    closegraph();
+    return 0;
+}
