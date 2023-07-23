64f42a0c5100f02e1ffd5a6b2bfdd900ae611f9b
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-07-23 17:55:10 +0530

    24. Write a Program for moving circle in different directions.

--- Lab_Program_List/Q24_circle_in_different_directions.cpp
+++ Lab_Program_List/Q24_circle_in_different_directions.cpp
@@ -3,3 +3,41 @@
 
  Write a Program for moving circle in different directions.
 */
+
+#include<graphics.h>
+int main()
+{
+    int x,y,i,f;
+    initwindow(1000,800,"Rajkishor");
+    x=400;
+    y=400;
+    for(i=0; i<5; i++)
+
+    while(f) {
+    cleardevice();
+    for(i=0; i<4; i++)
+    circle(x,y,100-i);
+
+    if(GetAsyncKeyState(VK_LEFT))
+    {
+            x-=5;
+    }
+    if(GetAsyncKeyState(VK_RIGHT)){
+        x+=5;
+    }
+    if(GetAsyncKeyState(VK_UP)){
+        y-=5;
+    }
+    if(GetAsyncKeyState(VK_DOWN)){
+        y+=5;
+    }
+    if(GetAsyncKeyState(VK_BACK)){
+        f=0;
+    }
+    delay(20);
+    }
+
+    getch();
+    closegraph();
+    return 0;
+}
