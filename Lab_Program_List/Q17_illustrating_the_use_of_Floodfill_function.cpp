1a9afb1d545319866d1461e67faba1fe52df9144
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-07-23 17:45:54 +0530

    17. Write a Program that illustrating the use of Floodfill function.

--- Lab_Program_List/Q17_illustrating_the_use_of_Floodfill_function.cpp
+++ Lab_Program_List/Q17_illustrating_the_use_of_Floodfill_function.cpp
@@ -2,3 +2,22 @@
                         Program: 17
     Write a Program that illustrating the use of Floodfill function.
 */
+
+#include <graphics.h>
+int main()
+{
+    int p, point[] = {400, 200, 350, 250, 370, 250, 300, 350, 360, 350,
+                      360, 350, 250, 450, 380, 450, 380, 500, 420, 500,
+                      420, 450, 550, 450, 440, 350, 500, 350, 430, 250,
+                      450, 250, 400, 200};
+    initwindow(900, 800, "Rajkishor");
+
+    p = (sizeof(point) / sizeof(int)) / 2;
+    setcolor(GREEN);
+    drawpoly(p, point);
+    setfillstyle(1, GREEN);
+    floodfill(400, 250, GREEN);
+    getch();
+    closegraph();
+    return 0;
+}
