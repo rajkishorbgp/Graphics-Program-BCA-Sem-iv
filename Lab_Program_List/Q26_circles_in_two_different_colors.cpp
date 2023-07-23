a73c2f2d070d448e0f1e0eeeb44093c4c39acd56
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-07-23 18:05:20 +0530

    26. Write a C program to draw circles in circles in two different colors .

--- Lab_Program_List/Q26_circles_in_two_different_colors.cpp
+++ Lab_Program_List/Q26_circles_in_two_different_colors.cpp
@@ -1,5 +1,18 @@
 /*
-                         Program: 25
+                         Program: 26
 
     Write a C program to draw circles in circles in two different colors .
 */
+
+#include <graphics.h>
+int main()
+{
+    initwindow(1200, 700, "RajKishor");
+    setcolor(YELLOW);
+    circle(300, 300, 200);
+    setcolor(BLUE);
+    circle(900, 300, 200);
+    getch();
+    closegraph();
+    return 0;
+}
